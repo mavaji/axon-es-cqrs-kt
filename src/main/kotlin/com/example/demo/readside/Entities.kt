74a46cf63a95b1bf4ea93d91464c806ab80e1388
Vahid Mavaji <mavaji@outlook.com>
2025-06-01 12:29:06 +1000

    update versions

--- src/main/kotlin/com/example/demo/readside/Entities.kt
+++ src/main/kotlin/com/example/demo/readside/Entities.kt
@@ -1,7 +1,7 @@
 package com.example.demo.readside
 
-import javax.persistence.Entity
-import javax.persistence.Id
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
 
 @Entity
 class AccountEntity {
